cf3ac48e087718a5f0a653f9e39348ac4b265c13
Tominaga <sk.termite@gmail.com>
2019-08-26 14:28:54 +0900

    Add script

---
+++ Assets/Scripts/TitleScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TitleScript : MonoBehaviour
+{
+    public void StartClick() {
+        SceneManager.LoadScene("Main");
+    }
+    public void EndClick() {
+        Quit(); 
+    }
+
+    private void Quit() {
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+        #elif UNITY_STANDALONE
+            UnityEngine.Application.Quit();
+        #endif
+    }
+
+}
